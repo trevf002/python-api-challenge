872bce293cbe691f568373703f853b8cd2ea8295
trevf002 <72757928+trevf002@users.noreply.github.com>
2021-06-11 12:44:27 -0700

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0be4ecb499ebe65ddaf5281b9246dd94"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA4_rGpBisAW2MP7K5HjyyKpjY1uXP4hIw"
